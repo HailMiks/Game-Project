4ff711847f74611a85053d59bc31f23f1df377ad
HailMikss <mikhailangelomistula@gmail.com>
2024-03-21 02:01:42 +0800

    Made Player Animations and SFX/Music

--- core/src/com/mygdx/entropy/GScreen.java
+++ core/src/com/mygdx/entropy/GScreen.java
@@ -3,10 +3,12 @@ package com.mygdx.entropy;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
@@ -20,26 +22,33 @@ public class GScreen extends ScreenAdapter {
 
     private OrthographicCamera camera;
     private SpriteBatch batch;
-    private Texture tex;
+    private Music music;
     private World world;
     private Box2DDebugRenderer box2dDebugRenderer;
 
     private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
     private TileMapHelper tileMapHelper;
 
+    private TextureAtlas atlas;
+
     // Game Objects
     private Player player;
 
-
     public GScreen(OrthographicCamera camera) {
+
         this.camera = camera;
         this.batch = new SpriteBatch();
-        this.tex = new Texture("down_walk.png");
         this.world = new World(new Vector2(0, 0), false);
         this.box2dDebugRenderer = new Box2DDebugRenderer();
 
         this.tileMapHelper = new TileMapHelper(this);
         this.orthogonalTiledMapRenderer = tileMapHelper.setupMap();
+
+        this.music = Gdx.audio.newMusic(Gdx.files.internal("audio/music_box.wav"));
+    
+        music.setLooping(true);
+        music.setVolume(0.5f);
+        music.play();
     }
 
     private void update() {
@@ -76,10 +85,17 @@ public class GScreen extends ScreenAdapter {
 
         this.batch.begin();
         // render the objects
-        batch.draw(tex, player.getBody().getPosition().x * Constants.PPM - (tex.getWidth() / 2), player.getBody().getPosition().y * Constants.PPM - (tex.getHeight() - 28 / 2));
+        TextureRegion currentFrame = player.getCurrentFrame();
+        batch.draw(currentFrame, 
+        player.getBody().getPosition().x * Constants.PPM - (currentFrame.getRegionWidth() / 2), 
+        player.getBody().getPosition().y * Constants.PPM - (currentFrame.getRegionHeight() - 28 / 2));
         
         this.batch.end();
-        // box2dDebugRenderer.render(world, camera.combined.scl(Constants.PPM));
+        box2dDebugRenderer.render(world, camera.combined.scl(Constants.PPM));
+    }
+
+    public TextureAtlas getAtlas() {
+        return atlas;
     }
 
     public World getWorld() {
@@ -93,9 +109,10 @@ public class GScreen extends ScreenAdapter {
     @Override
     public void dispose() {
         batch.dispose();
-        tex.dispose();
         box2dDebugRenderer.dispose();
         orthogonalTiledMapRenderer.dispose();
         world.dispose();
+        music.dispose();
+        atlas.dispose();
     }
 }
--- core/src/com/mygdx/entropy/Objects/Player/Player.java
+++ core/src/com/mygdx/entropy/Objects/Player/Player.java
@@ -2,28 +2,154 @@ package com.mygdx.entropy.Objects.Player;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.mygdx.entropy.Utils.Constants;
 
 public class Player extends GameEntity {
 
+    private Sound footsteps;
+    private long footstepsId;
+    private boolean footstepPlaying = false;
+
+    private static final float FRAME_TIME = 1 / 4f;
+    private float elapsedTime;
+    private TextureAtlas atlas;
+    private Animation<TextureRegion> upStill, downStill, leftStill, rightStill, walkUp, walkDown, walkLeft, walkRight;
+    private Direction lastDirection = Direction.DOWN;
+
     public Player(float width, float height, Body body) {
         super(width, height, body);
         this.speed = 2.5f;
+        this.atlas = new TextureAtlas("player/lumianMove.atlas");
+        this.walkUp = new Animation<TextureRegion>(FRAME_TIME, atlas.findRegions("up_walk"));
+        this.walkDown = new Animation<TextureRegion>(FRAME_TIME, atlas.findRegions("down_walk"));
+        this.walkLeft = new Animation<TextureRegion>(FRAME_TIME, atlas.findRegions("left_walk"));
+        this.walkRight = new Animation<TextureRegion>(FRAME_TIME, atlas.findRegions("right_walk"));
+        this.upStill = new Animation<TextureRegion>(FRAME_TIME, atlas.findRegions("up_still"));
+        this.downStill = new Animation<TextureRegion>(FRAME_TIME, atlas.findRegions("down_still"));
+        this.leftStill = new Animation<TextureRegion>(FRAME_TIME, atlas.findRegions("left_still"));
+        this.rightStill = new Animation<TextureRegion>(FRAME_TIME, atlas.findRegions("right_still"));
+    
+        footsteps = Gdx.audio.newSound(Gdx.files.internal("audio/walkSFX.mp3"));
+    }
+
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
     }
 
     @Override
     public void update() {
         x = body.getPosition().x * Constants.PPM;
         y = body.getPosition().y * Constants.PPM; 
+        elapsedTime += Gdx.graphics.getDeltaTime();
+
+        boolean isMoving = (velX != 0 || velY != 0);
+
+        // Check the direction of movement
+        if (isMoving) {
+            if (velX > 0) {
+                lastDirection = Direction.RIGHT;
+                if (!footstepPlaying) {
+                    footstepsId = footsteps.loop(0.4f, 0.8f, 0);
+                    footstepPlaying = true;
+                }
+            } else if (velX < 0) {
+                lastDirection = Direction.LEFT;
+                if (!footstepPlaying) {
+                    footstepsId = footsteps.loop(0.4f, 0.8f, 0);
+                    footstepPlaying = true;
+                }
+            } else if (velY > 0) {
+                lastDirection = Direction.UP;
+                if (!footstepPlaying) {
+                    footstepsId = footsteps.loop(0.4f, 0.8f, 0);
+                    footstepPlaying = true;
+                }
+            } else if (velY < 0) {
+                lastDirection = Direction.DOWN;
+                if (!footstepPlaying) {
+                    footstepsId = footsteps.loop(0.4f, 0.8f, 0);
+                    footstepPlaying = true;
+                }
+            }
+        } else {
+            // Stop footstep sound if the player is not moving
+            if (footstepPlaying) {
+                footsteps.stop(footstepsId);
+                footstepPlaying = false;
+            }
+        }
 
         checkUserInput();
     }
 
     @Override
     public void render(SpriteBatch batch) {
+        TextureRegion currentFrame;
+        if (velX > 0)
+            currentFrame = walkRight.getKeyFrame(elapsedTime, true);
+        else if (velX < 0)
+            currentFrame = walkLeft.getKeyFrame(elapsedTime, true);
+        else if (velY > 0)
+            currentFrame = walkUp.getKeyFrame(elapsedTime, true);
+        else if (velY < 0)
+            currentFrame = walkDown.getKeyFrame(elapsedTime, true);
+        else {
+            switch (lastDirection) {
+                 // Use the first frame for the direction
+                case UP:
+                    currentFrame = upStill.getKeyFrame(0);
+                    break;
+                case DOWN:
+                    currentFrame = downStill.getKeyFrame(0);
+                    break;
+                case LEFT:
+                    currentFrame = leftStill.getKeyFrame(0); 
+                    break;
+                case RIGHT:
+                    currentFrame = rightStill.getKeyFrame(0); 
+                    break;
+                default:
+                    currentFrame = downStill.getKeyFrame(0, true); 
+            }
+        }
+            
 
+        batch.draw(currentFrame, x, y);
+    }
+
+    public TextureRegion getCurrentFrame() {
+        if (velX > 0)
+            return walkRight.getKeyFrame(elapsedTime, true);
+        else if (velX < 0)
+            return walkLeft.getKeyFrame(elapsedTime, true);
+        else if (velY > 0)
+            return walkUp.getKeyFrame(elapsedTime, true);
+        else if (velY < 0)
+            return walkDown.getKeyFrame(elapsedTime, true);
+        else {
+            switch (lastDirection) {
+                // Return first frame of animation
+                case UP:
+                    return upStill.getKeyFrame(0, true); 
+                case DOWN:
+                    return downStill.getKeyFrame(0, true); 
+                case LEFT:
+                    return leftStill.getKeyFrame(0, true); 
+                case RIGHT:
+                    return rightStill.getKeyFrame(0, true);
+                default:
+                    return downStill.getKeyFrame(0, true); 
+            }
+        }
     }
 
     private void checkUserInput() {
@@ -38,6 +164,20 @@ public class Player extends GameEntity {
         if(Gdx.input.isKeyPressed(Input.Keys.S))
          velY = -1;
 
+
+        if (velX > 0)
+            lastDirection = Direction.RIGHT;
+        else if (velX < 0)
+            lastDirection = Direction.LEFT;
+        else if (velY > 0)
+            lastDirection = Direction.UP;
+        else if (velY < 0)
+            lastDirection = Direction.DOWN;
         body.setLinearVelocity(velX * speed, velY * speed);
+    }
+
+    public void dispose() {
+        atlas.dispose();
+        footsteps.dispose();        
     }
 }
